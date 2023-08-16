16fde4b9f8a64efbb1bede783654c745f34602a6
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-08-16 10:23:34 +0100

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, StarCanvas} from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  // Tech,
+  Works,
+  StarsCanvas,
+  StarCanvas,
+} from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
 
-          <div className='relative z-0'>
-          <Hero />
-          <StarCanvas />
+          <div className="relative z-0">
+            <Hero />
+            <StarCanvas />
+          </div>
         </div>
 
-        </div>
-        
         <About />
         <Experience />
         {/* <Tech /> */}
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
